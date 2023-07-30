8653e95fd18157e5d3545f0addcb9f47165e8797
EdHloba <hloba.e.v@gmail.com>
2023-07-30 21:11:08 +0300

    create components+pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import MoviesList from "components/MoviesList";
+
+const Home = () => {
+    return <div>
+        <h2>Trending today</h2>
+        <MoviesList/>
+    </div>
+}
+
+export default Home;
